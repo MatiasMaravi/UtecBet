09993bd949da5b2ee1c6375d25c4244b21dc25fc
unknown <matias.maravi@utec.edu.pe>
2022-09-23 15:43:53 -0500

    Creacion de la base de datos con nombre, contraseña y cuenta de 5000

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+from curses.ascii import US
 from unittest import result
 from flask import (
     Flask, 
@@ -12,7 +12,7 @@ from flask_migrate import Migrate
 
 #Configuration
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432/utecbet2022'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jerimy:12345@localhost:5432/utecbet2022'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -22,13 +22,12 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
-    password = db.Column(db.Boolean, nullable=False, default=False)
-    cash = db.Column(db.Integer, nullable=False,default=0)
+    password = db.Column(db.String(), nullable=False)
+    cash = db.Column(db.Integer, nullable=False,default=5000)
     def __repr__(self):
         return f'User: id={self.id}, name={self.name}, password={self.password}, cash={self.cash}'
         
-
-class  Team(db.Model):
+class Team(db.Model):
     __tablename__ = 'teams'
     name = db.Column(db.String(),primary_key = True)
     winrate = db.Column(db.Float,nullable = False,default = 0)
@@ -39,10 +38,24 @@ db.create_all()
 
 #Controllers
 @app.route('/', methods=['GET'])
-def index():
-    return "hola"
+def greetings():
+    return render_template('index.html')
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        name = request.args.get('name')
+        password = request.args.get('password')
+    else:
+        name = request.form.get('name')
+        password = request.form.get('password')
+
+    user = User(name=name, password=password)
+    db.session.add(user)
+    db.session.commit()
+    
+    return_str = '{} {}'.format(name, "********")
+    return render_template('thankyou.html', data=return_str)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
