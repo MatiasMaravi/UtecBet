8a1c0f57690aa1ca59587964aed12284c1d0d2e0
Jerimy2021 <jerimy.sandoval@utec.edu.pe>
2022-10-11 11:29:35 -0500

    Inicio de sesion

--- app.py
+++ app.py
@@ -1,44 +1,46 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy  import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
-from curses.ascii import US
-from tabnanny import check
-from unittest import result
-from flask import (
-    Flask, 
-    render_template,
-    request
-)
-from flask_sqlalchemy import SQLAlchemy
-
-from werkzeug.security import check_password_hash,generate_password_hash
-
-from config import config
-from flask_wtf import CSRFProtect
-import os
-
-#Configuration
+##Modelos
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jerimy:12345@localhost:5432/utecbet2022'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-app.config
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
-csrf = CSRFProtect(app)
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-@classmethod
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(), unique=True, nullable=False)
-    password = db.Column(db.String(), unique=True, nullable=False)
+    username = db.Column(db.String(255), unique=True)
+    email = db.Column(db.String(255))
+    password = db.Column(db.String(255))
     cash = db.Column(db.Integer, default=5000,nullable=False)
-    def check_password(self, hashed_password,password):
-        return check_password_hash(hashed_password,password)
-    def __repr__(self):
-        return f'User: id={self.id}, name={self.name}, password={self.password}, cash={self.cash}'
-        
+
+#login_manager
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+    
 class Team(db.Model):
     __tablename__ = 'teams'
     name = db.Column(db.String(),primary_key = True)
@@ -67,22 +69,59 @@ class Transaccion(db.Model):
     id_transaccion=db.relationship('BET', backref='transaccion',lazy=True)
     def __repr__(self):
         return f'Transaccion: id={self.id}, name={self.name}, password={self.password}, cash={self.cash}, id_transaccion={self.id_transaccion}'
-
 db.create_all()
 
-#Controllers
+
+##Endpoints
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method=='POST':
-        print(request.form['username'])
-        print(request.form['password'])
-        
-    return render_template('auth/login.html')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                ##retornar a UtecBEt
+                return redirect(url_for('dashboard'))
+        ##retornar 
+        else:
+            print('<h1>Invalid username or password</h1>')
+            return render_template('index.html')
+       
+
+    return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return '<h1>New user has been created!</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('signup.html', form=form)
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name=current_user.username)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.config.from_object(config['development'])
     app.run(debug=True)
-    csrf = CSRFProtect(app)
-
-
