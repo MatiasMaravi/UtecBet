5a4ff6b812dc5640c4aa16c7561618c90f3d61fc
Matias Maravi <matias.maravi@utec.edu.pe>
2022-10-13 10:10:39 -0500

    login creado

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 
 ##Modelos
 app = Flask(__name__)
+user = "postgres:123"
+data_base = "utecbet2022"
+conection = "localhost:5432"
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jerimy:12345@localhost:5432/utecbet2022'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}@{conection}/{data_base}'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
@@ -51,19 +54,19 @@ class Team(db.Model):
     def __repr__(self):
         return f'Team: name={self.name}, winrate={self.winrate}, coach={self.coach}'
 
-class BET(db.Model):
-    __tablename__ = 'bet'
+class Bet(db.Model):
+    __tablename__ = 'bets'
     posible_ganador = db.Column(db.String(), nullable=False)
     cuota = db.Column(db.Float, nullable=False, default=1.00)
     resultado = db.Column(db.String(), nullable=False)
     monto_apuesta = db.Column(db.Integer, nullable=False)
     M_codigo= db.Column(db.Integer, primary_key = True)
-    C_transaccion= db.Column(db.Integer, db.ForeignKey('transaccion.id'), nullable=False)
+    C_transaccion= db.Column(db.Integer, db.ForeignKey('transacciones.id'), nullable=False)
     def __repr__(self):
         return f'BET: posible_ganador={self.posible_ganador}, cuota={self.cuota}, resultado={self.resultado}, monto_apuesta={self.monto_apuesta}, M_codigo={self.M_codigo}'
 
 class Transaccion(db.Model):
-    __tablename__ = 'transaccion'
+    __tablename__ = 'transacciones'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
     password = db.Column(db.String(), nullable=False)
@@ -71,8 +74,9 @@ class Transaccion(db.Model):
     id_transaccion=db.relationship('BET', backref='transaccion',lazy=True)
     def __repr__(self):
         return f'Transaccion: id={self.id}, name={self.name}, password={self.password}, cash={self.cash}, id_transaccion={self.id_transaccion}'
-db.create_all()
 
+with app.app_context():
+    db.create_all()
 
 ##Endpoints
 
