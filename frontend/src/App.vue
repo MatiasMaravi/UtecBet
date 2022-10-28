873d050affd9b90d57f1332849375bb2fad4357c
Jerimy2021 <jerimy.sandoval@utec.edu.pe>
2022-10-28 08:59:27 -0500

    navigation

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <NavComponent />
+  <router-view />
+</template>
+<script>
+import NavComponent from './components/NavComponent.vue'
+
+export default {
+  components: {
+    NavComponent,
+  },
+}
+</script>
+<style>
+#app {
+  color: #2c3e50;
+}
+</style>
